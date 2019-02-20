cd68b8dcd26a9c2f0d8823ac45caa15f601c21f0
Alex Xiao <alex.hongquan.xiao@gmail.com>
2019-02-19 23:10:05 -0800

    add header to profile and question

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,40 +1,40 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './Header.scss';
 
-class Header extends Component {
-    gotoProfile = () => {
+const gotoProfile = () => {
+    console.log('aaa')
+};
 
-    };
+const Header = () => {
 
-    render() {
-        return (
-            <div className='headerContainer'>
-                <div className='container-fluid'>
-                    <div className='row'>
-                        <div className='col-sm-3'>
-                            img here
-                        </div>
 
-                        <div className='col-sm-6'>
-                            <div className="input-group">
-                                <input className='form-control'/>
+    return (
+        <div className='headerContainer'>
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-sm-3'>
+                        img here
+                    </div>
 
-                                <div className="input-group-append">
-                                    <button className="btn btn-outline-light" type="button">Search</button>
-                                </div>
+                    <div className='col-sm-6'>
+                        <div className="input-group">
+                            <input className='form-control'/>
+
+                            <div className="input-group-append">
+                                <button className="btn btn-outline-light" type="button">Search</button>
                             </div>
                         </div>
+                    </div>
 
-                        <div className='col-sm-3'>
-                            <div className='profileImg' onClick={this.gotoProfile}>
+                    <div className='col-sm-3'>
+                        <div className='profileImg' onClick={gotoProfile}>
 
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default Header;
