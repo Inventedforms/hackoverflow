1a8908211771ec66a63afbd40cce5d70d801cd73
rprakash2 <rishabh.prakash@elliemae.com>
2019-02-19 23:46:38 -0800

    API Integration for Profile page - attempted..

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -3,6 +3,8 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 import UserInfo from './UserInfo';
 import Skills from './Skills';
 import Header from './../Header/Header';
+import ApiService from '../../common/services/ApiService';
+
 import "./Profile.css";
 
 export default class Profile extends Component{
@@ -10,6 +12,8 @@ export default class Profile extends Component{
         super(props);
        // console.log('user id here', this.props.match.params.userId)
 
+        
+    
     this.state = {
             displayName: "Rishabh Prakash",
             username: "rprakash",
@@ -24,23 +28,25 @@ export default class Profile extends Component{
 
     componentDidMount() {
 
-        let user = {
-            displayName: "Rishabh Prakash",
-            username: "rprakash",
-            profilePicUrl: "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-            skills: [
-                "test1",
-                "test2",
-                "test3"
-            ]
-        };
-
-        this.setState({
-            name: user.displayName,
-            displayName: user.displayName,
-            profilePicUrl: user.profilePicUrl,
-            skills: user.skills
-        })
+        
+        let url = 'http://977051ba.ngrok.io/users/5c6c66915abceabc53933c96' 
+        
+        fetch(url, {
+            method: 'GET',
+            header: {
+                'Content-Type': 'application/json'
+            }
+        })  .then(res => res.json())
+            .then(json => { 
+                console.log(json);
+                this.setState({ 
+                    name: json.name,
+                    displayName: json.screen_name,
+                    organization: json.organization,
+                    profilePicUrl:  json.profile_picture,
+                    skills: json.tags
+                 })
+        });
     }
 
     render() {
@@ -48,7 +54,7 @@ export default class Profile extends Component{
             <React.Fragment>
                 <Header/>
 
-                <div style={{marginTop: '60px'}}>
+                <div style={{marginTop: '60px'}} className = "UserProfile">
                     <Container>
                         <Row>
                             <Col sm = {12}>
