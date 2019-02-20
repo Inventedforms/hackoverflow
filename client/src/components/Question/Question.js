41525f2017cf43ce31a3cf2093c9a58bd5f080c5
Rajat Arora <rajat.arora@elliemae.com>
2019-02-20 00:21:10 -0800

    Dangerous stuff

--- client/src/components/Question/Question.js
+++ client/src/components/Question/Question.js
@@ -1,26 +1,43 @@
 import React, {Component} from 'react';
 import Header from './../Header/Header';
+import SingleQuestion from './SingleQuestion'
 
 class Question extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+        }
+    }
 
+    componentDidMount() {
+
+        fetch('http://localhost:5000/threads')
+        .then(res => res.json())
+        .then(json => {
+            this.setState({ threads: json })
+        });
+       
     }
 
     render() {
-        return (
-            <React.Fragment>
+        if (this.state.threads) {
+            return (
+                <React.Fragment>
                 <Header/>
-
-                <div style={{marginTop: '60px'}}>
-                    This is question
-                </div>
-            </React.Fragment>
-
-
-        );
-
-
+                    <div style={{marginTop: '60px'}}>
+                        <SingleQuestion threads={this.state.threads} />
+                    </div>
+                </React.Fragment>
+            );
+    
+        } else {
+            return (
+                <React.Fragment>
+                <Header/>
+                <div></div>
+                </React.Fragment>
+            )
+        }
     }
 }
 
